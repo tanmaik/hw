41f27964a55bbd042934f5b3888b1afec35581ad
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2024-12-04 22:20:41 -0500

    FORCE RESET

--- web/utils/uploadthing.ts
+++
@@ -1,9 +0,0 @@
-import {
-  generateUploadButton,
-  generateUploadDropzone,
-} from "@uploadthing/react";
-
-import type { fileRouter } from "@/app/api/uploadthing/core";
-
-export const UploadButton = generateUploadButton<typeof fileRouter>();
-export const UploadDropzone = generateUploadDropzone<typeof fileRouter>();
