33e6632cbfba863ca856529b34450a564870077c
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2024-12-24 16:58:54 -0500

    fully configure auth

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,9 +1,12 @@
-export default function Home() {
-
-
-  return (
-    <div>
-      <button>create new class</button>
-    </div>
-  );
+import { auth } from "@/auth";
+import SignIn from "@/components/sign-in";
+export default async function Home() {
+  const session = await auth();
+  if (!session?.user)
+    return (
+      <div>
+        you are not signed in. please <SignIn />
+      </div>
+    );
+  return <div>you are signed in {JSON.stringify(session)}</div>;
 }
