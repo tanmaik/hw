e0e0d13f364b6a7e93efab5979c2bc60f1cd52a7
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2024-12-25 18:51:32 -0500

    remove user data collection from prisma

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,8 +1,13 @@
+
+
 export default async function Home() {
+
   return (
     <div className="p-4">
       <div className="py-10 bg-black w-full"></div>
       <h1 className="font-serif mt-4 font-bold">THE HOMEWORK MACHINE</h1>
+      <p className="mt-4">Your classes</p>
+      {}
     </div>
   );
 }
