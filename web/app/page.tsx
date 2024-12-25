768672b1c8f3a43ac5552d08d0123b20b2e18622
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2024-12-25 18:49:22 -0500

    removed auth

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,35 +1,8 @@
-import { auth, signOut } from "@/auth";
-import { redirect } from "next/navigation";
-
-import { prisma } from "@/lib/db";
 export default async function Home() {
-  const session = await auth();
-  if (!session?.user) redirect("/login");
-
-  let user = await prisma.user.findFirst({
-    where: { email: session.user.email },
-  });
-
-  if (!user) {
-    user = await prisma.user.create({
-      data: {
-        name: session.user.name,
-        email: session.user.email,
-      },
-    });
-    console.log(user);
-  }
-
-  const allUsers = await prisma.user.findMany({});
-  const numUsers = await prisma.user.count();
   return (
-    <div>
-      {allUsers.map((user) => (
-        <div key={user.id}>
-          {user.email} | {user.name}{" "}
-        </div>
-      ))}
-      dsjfaklsdjfs {numUsers}
+    <div className="p-4">
+      <div className="py-10 bg-black w-full"></div>
+      <h1 className="font-serif mt-4 font-bold">THE HOMEWORK MACHINE</h1>
     </div>
   );
 }
