5cc56d9f38622202fd481702c12d9e4a80b1f1f4
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2024-12-25 23:43:35 -0500

    prompt experimentation

--- ai/browser-test.py
+++ ai/browser-test.py
@@ -8,15 +8,27 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import asyncio
 
-from browser_use import Agent, Browser, Controller
+from browser_use import Agent, Browser, Controller, SystemPrompt
 from browser_use.browser.browser import BrowserConfig
-
+from pydantic import BaseModel, Field
+from typing import Optional
 
 controller = Controller()
 
-@controller.action('Ask human to sign in once you see the Duo screen')
-def ask_human_to_sign_in() -> str:
-  return input(f'\n\nInput: ')
+class Assignment(BaseModel):
+    name: str
+    due_date: Optional[str]
+    assignment_details: str = Field(description='Should include all details of what to do for the assignment, the rubric, etc. A student should be able to do the whole assignment based on solely this information, so make sure it is very detailed and comprehensive.')
+   
+
+@controller.action('Ask human to bypass a screen')
+def ask_human_to_bypass() -> str:
+  return input(f'\n\nPress enter to continue')
+
+@controller.action('Save an assignment to a file', param_model=Assignment)
+def save_assignment(assignment: Assignment) -> str:
+    print(assignment)
+
 
 async def main():
     browser = Browser(config=BrowserConfig(chrome_instance_path='/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'))
@@ -25,8 +37,16 @@ async def main():
 
       model = ChatOpenAI(model='gpt-4o')
    
+      task = '''Open https://canvas.its.virginia.edu and look for all the assignments for the course CS3130. An assignment is anything that is due. 
+      Explore all parts of the website like you are a student and explore the website. 
+      You may leave the Canvas website to find more information.
+      Do not stop until you know that you have looked at all the information related to this course.
+      Every time you find an assignment, save it to a file using save_assignment; dig into each assignment further to make sure that you are getting the full details to do the assignment. We should never have to refer to the actual teacher given assignment details, because your description should be that good.
+      If you get to a login screen (or a screen that you cannot pass), use ask_human_to_bypass to ask the human to sign in.
+      '''
+    
       agent1 = Agent(
-            task='Open canvas.its.virginia.edu and go through each of the classes and find the teachers names. You can ask me to login when you get to an authentication screen. ',
+            task=task,
             llm=model,
             browser_context=context,
             controller=controller
