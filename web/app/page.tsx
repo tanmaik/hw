aa0084ca7e4e6defcc4d20125ea10d109720162e
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2024-12-25 19:19:14 -0500

    fix fr

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,28 +1,18 @@
 import { prisma } from "@/lib/db";
 import Link from "next/link";
-import { revalidatePath } from "next/cache";
 
 export default async function Home() {
   const classes = await prisma.class.findMany({});
-
-  async function removeClass(formData: FormData) {
-    "use server";
-    const classId = parseInt(formData.get("classId") as string);
-    await prisma.class.delete({
-      where: { id: classId },
-    });
-    revalidatePath("/");
-  }
-
+  // console.log(classes);
   return (
     <div className="p-2">
       <div className="py-10 bg-black w-full font-mono"></div>
       <div className="flex flex-col max-w-[15rem]">
         <h1 className="mt-4 text-2xl font-bold">Student Simulator</h1>
-        <div className="flex items-center justify-between mt-4">
+        <div className="flex items-center justify-between mt-4 mb-1">
           <p className="font-semibold">Your classes</p>
           <Link href="/add-class">
-            <button className="font-sans tracking-tight px-0.5 py-0.5 text-black bg-gradient-to-b from-neutral-200 via-neutral-300 to-neutral-500 hover:border-neutral-400 border border-black rounded-sm font-normal text-sm inline-block active:scale-95 active:shadow-inner transition-all">
+            <button className="font-sans tracking-tight px-0.5  text-black bg-gradient-to-b from-neutral-200 via-neutral-200 to-neutral-400 hover:border-neutral-400 border border-black rounded-sm font-normal text-sm inline-block active:scale-95 active:shadow-inner transition-all">
               add class
             </button>
           </Link>
@@ -31,21 +21,10 @@ export default async function Home() {
         <div className="mt-1">
           {classes.length > 0 ? (
             classes.map((classItem) => (
-              <div
-                key={classItem.id}
-                className="flex items-center justify-between mt-1"
-              >
-                <div>{classItem.name}</div>
-                <form action={removeClass}>
-                  <input type="hidden" name="classId" value={classItem.id} />
-                  <button className="font-sans tracking-tight px-0.5 py-0.5 text-black bg-gradient-to-b from-neutral-200 via-neutral-300 to-neutral-500 hover:border-neutral-400 border border-black rounded-sm font-normal text-sm inline-block active:scale-95 active:shadow-inner transition-all">
-                    remove
-                  </button>
-                </form>
-              </div>
+              <div key={classItem.id}>{classItem.name}</div>
             ))
           ) : (
-            <div className="mt-1">No classes</div>
+            <span>No classes</span>
           )}
         </div>
       </div>
